ac252c416f6f15f66edddfaa82a29ae7171c7598
UNCROOS <any492266@gmail.com>
2024-08-02 17:57:19 +0900

    init

---
+++ clowing/lib/screens/start/gender_screen.dart
@@ -0,0 +1,120 @@
+import 'package:clowing/screens/start/login_successful_screen.dart';
+import 'package:flutter/material.dart';
+
+class GenderScreen extends StatefulWidget {
+  @override
+  _GenderScreenState createState() => _GenderScreenState();
+}
+
+class _GenderScreenState extends State<GenderScreen> {
+  String? _selectedGender; // Changed to String? to handle null values
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      backgroundColor:
+          Color.fromARGB(255, 255, 255, 255), // Light blue background color
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              '성별을\n선택해주세요!',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 50),
+            Text(
+              '성별',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            DropdownButton<String>(
+              isExpanded: true,
+              value: _selectedGender,
+              hint: Text('성별을 선택하세요'),
+              items: <DropdownMenuItem<String>>[
+                DropdownMenuItem<String>(
+                  value: '여자',
+                  child: Text('여자'),
+                ),
+                DropdownMenuItem<String>(
+                  value: '남자',
+                  child: Text('남자'),
+                ),
+                DropdownMenuItem<String>(
+                  value: '기타',
+                  child: Text('기타'),
+                ),
+              ],
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedGender = newValue;
+                });
+              },
+            ),
+            Spacer(), // This ensures the button stays at the bottom of the screen
+            nextPageButton(), // Call the method to display the button
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget nextPageButton() {
+    return InkWell(
+      onTap: () {
+        // Navigate to the login successful screen
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => LoginSuccessfulScreen()),
+        );
+      },
+      child: Card(
+        margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
+        elevation: 2,
+        child: Container(
+          height: 50,
+          decoration: BoxDecoration(
+            color: Color(0xFFC9DEFF),
+            borderRadius: BorderRadius.circular(7),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(width: 10),
+              const Text(
+                "다음",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(width: 10),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: GenderScreen(),
+  ));
+}
--- clowing/lib/screens/start/login_successful_screen.dart
+++ clowing/lib/screens/start/login_successful_screen.dart
@@ -1,11 +1,12 @@
+import 'package:clowing/screens/mypage_screen.dart';
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatefulWidget {
+class LoginSuccessfulScreen extends StatefulWidget {
   @override
-  _StartScreenState createState() => _StartScreenState();
+  _LoginSuccessfulScreenState createState() => _LoginSuccessfulScreenState();
 }
 
-class _StartScreenState extends State<StartScreen> {
+class _LoginSuccessfulScreenState extends State<LoginSuccessfulScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,44 +16,54 @@ class _StartScreenState extends State<StartScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // More flexible space at the top
             Image.asset(
               'assets/images/F2.png', // Replace with your logo image asset path
               width: 250,
               height: 250,
             ),
+            const SizedBox(height: 20), // Adds space between image and button
+            nextPageButton(),
           ],
         ),
       ),
     );
   }
-}
 
-Widget nextPageButton() {
-  return InkWell(
-    child: Card(
-      margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
-      elevation: 2,
-      child: Container(
-        height: 50,
-        decoration: BoxDecoration(
-          color: Color(0xFF80ABEE),
-          borderRadius: BorderRadius.circular(7),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(width: 10),
-            const SizedBox(width: 10),
-            const Text(
-              "다음",
-              style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255), fontSize: 17),
-            ),
-          ],
+  Widget nextPageButton() {
+    return InkWell(
+      onTap: () {
+        // Navigate to the login successful screen
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MyPageScreen()),
+        );
+      },
+      child: Card(
+        margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
+        elevation: 2,
+        child: Container(
+          height: 50,
+          decoration: BoxDecoration(
+            color: Color(0xFFC9DEFF),
+            borderRadius: BorderRadius.circular(7),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(width: 10),
+              const SizedBox(width: 10),
+              const Text(
+                "클로윙 시작하기",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
